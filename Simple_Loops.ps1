1780f99f9c48ea19c2c8e84a6e90a0134c47e06c
Aslam Admani <35896884+aslamadmani1337@users.noreply.github.com>
2022-03-24 09:45:05 +0000

    Create Simple_Loops.ps1
    this is where it all starts

---
+++ Simple_Loops.ps1
@@ -0,0 +1,36 @@
+# practising powershell
+
+# for (int)x; x count; x increment|decrement
+for($i=1; $i -lt 10; $i++)
+{
+  write-host "$i"
+}
+
+# square bracket for casting
+[int]$numberOfPlayers = Read-Host "How many players do you want"
+$players = @()
+
+for ($c=1; $c -ne $numberOfPlayers+1; $c++)
+{
+  $players += "player $c"
+}
+write-host $players
+
+$regexCheck = '^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+# IPv4 address ^
+$list = @()
+# for each X (*in*) Y
+foreach($line in cat C:\Users\user\Desktop\file.txt)
+# modify the filepath
+{
+  if ($line -match $regexCheck)
+  {
+    write-host "adding: $line to list"
+    $list += $line
+  }
+  else
+  {
+    continue
+  }
+}
+
