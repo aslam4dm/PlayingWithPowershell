a6ecef1902977f58cc5be1f3c76f785960374930
Aslam Admani <35896884+aslamadmani1337@users.noreply.github.com>
2022-03-24 15:27:17 +0000

    Create FindHrefs.ps1
    Makes a get request to specified website, downloads html response and saves to file.
    Reads through file to find hrefs 
    
    Has 2 lists, one list that contains all Hrefs, another that looks for target domain specific hrefs (self-references)

---
+++ FindHrefs.ps1
@@ -0,0 +1,61 @@
+# Modify to take arguments - where user can choose whether they want to collect all domains, subdomains or specific target domains 
+# Use functions to better organise code
+
+$uri = Read-Host "Enter a valid URI"
+
+
+ if ($uri.contains("."))
+ {
+   $uriBaseTemp = @($uri.split("`."))
+ }
+ $uriBase = $uriBaseTemp[0]
+
+ $outfile = "output.txt"
+ [int]$numOfLinesWithUri = 0
+
+ Invoke-WebRequest -uri $uri -outfile $outfile
+
+ foreach($line in cat $outfile)
+ {
+   if ($line.contains($uri))
+   {
+     $numOfLinesWithUri += 1
+   }
+ }
+
+ # Search for hrefs e.g. abcd href="https://example.com/q?query=getthis"<><>...
+ $allHrefs = @()
+ $targetHrefs = @()
+ $s1 = cat $outfile
+ $search = $s1 -split "href=`""
+   foreach ($u in $search)
+   {
+     # adding all hrefs
+     if ($u.startswith("http"))
+     {
+       # add everything from 0th element up to next quotation mark (")
+       # https:example.com/q?query=getthis
+       $allHrefs += $u.split("`"")[0]
+     }
+     else
+     {
+       continue
+     }
+   }
+
+   # https://www.example.co.uk/q?...
+   # http://account.example.co.uk/q?...
+   foreach ($u in $allHrefs)
+   {
+     $uris = $u -split "://"
+
+     if (($uris[1].startswith("www.$uriBase")) -or ($uris[1].startswith($uriBase)))
+     {
+         $targetHrefs += $uris[1]
+     }
+     else {continue}
+   }
+
+ foreach ($l in $allHrefs){write-host $l}
+ write-host "`n"
+ foreach ($l in $targetHrefs){write-host $l}
